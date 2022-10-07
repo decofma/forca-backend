5c46d3ad4cb11acd33a977d7a755474c4b4c4eae
André Antunes <andrem.ferraz@hotmail.com>
2022-10-07 00:42:20 -0300

    v0.01

--- server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/teste')
-def hello_world():
-    return {"teste": ["teste1", "teste2", "teste3"]}
-
-if __name__ == "__main__":
-    app.run(debug=True)
